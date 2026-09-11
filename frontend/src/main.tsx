e69039269602865028e31d249684cd2a53f9df19
Unlucky70Ladybug <unhappywrld417@gmail.com>
2026-09-11 15:02:16 +0900

    add:ホームページの作成

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/home/home";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        { /* <Route path="/login" element={<Login />} /> */}
+      </Routes>
+    </Router>
   </StrictMode>,
-)
+);
