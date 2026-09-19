303a14cf72c48f3663ce0610203fedf20e23d71c
Unlucky70Ladybug <unhappywrld417@gmail.com>
2026-09-19 16:57:26 +0900

    fix:carrierwaveの設定変更

--- backend/app/controllers/auth/sessions_controller.rb
+++ backend/app/controllers/auth/sessions_controller.rb
@@ -1,9 +1,19 @@
 class Auth::SessionsController < ApplicationController
-    def index
-        if current_user
-            render json: {is_login: true, data: current_user }
-        else
-            render json: {is_login: false, message: "ユーザーが存在しません"}
-        end
+  def index
+    if current_user
+      render json: {
+        is_login: true,
+        user: {
+          id: current_user.id,
+          name: current_user.name,
+          avatarImage: current_user.avatar_image.url
+        }
+      }
+    else
+      render json: {
+        is_login: false,
+        message: "ユーザーが存在しません"
+      }
     end
+  end
 end
