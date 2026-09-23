dd301de44db2c44be351c3b7c2589ccfb4db0dce
Unlucky70Ladybug <unhappywrld417@gmail.com>
2026-09-23 22:01:20 +0900

    add:書籍検索API用のコントローラ作成

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
     resources :sessions, only: %i[index]
   end
 
+  namespace :api do
+    namespace :v1 do
+      
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
